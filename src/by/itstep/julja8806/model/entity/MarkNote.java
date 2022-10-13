24838a3633293cc0f4adeedf88e55f8b667ef44f
yalo24 <olgarizkovic@gmail.com>
2022-10-13 11:49:31 +0300

    Composition

---
+++ src/by/itstep/julja8806/model/entity/MarkNote.java
@@ -0,0 +1,23 @@
+package by.itstep.julja8806.model.entity;
+
+public class MarkNote {
+    private double mark;
+
+    public MarkNote() {
+        mark = 4;
+    }
+
+    public MarkNote(double mark) {
+
+        this.mark = mark;
+    }
+
+    public double getMark() {
+        return mark;
+    }
+
+    public void setMark(double mark) {
+        this.mark = mark;
+    }
+
+}
--- src/by/itstep/julja8806/model/entity/Student.java
+++ src/by/itstep/julja8806/model/entity/Student.java
@@ -4,23 +4,26 @@ public class Student extends Human {
     public static final int MIN_MARK = 0;
     public static final int MAX_MARK = 10;
     private double mark;
+    private MarkNote note;
 
     public Student() {
+
 
     }
 
     public Student(String name, int age, double mark) {
         super(name, age);
         this.mark = mark;
+        note = new MarkNote(mark);
     }
 
     public double getMark() {
-        return mark;
+        return note.getMark();
     }
 
     public void setMark(double mark) {
         if (mark >= MIN_MARK && mark <= MAX_MARK) {
-            this.mark = mark;
+            note.setMark(mark);
         }
     }
 
